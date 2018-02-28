ff8eebd64bd4963bc4dc5e540a200420fe32e487
François Darmon <fra.darmon@gmail.com>
2018-02-28 17:34:57 +0100

    Implementation of LogReg, not tested yet

--- src/Classifier.py
+++ src/Classifier.py
@@ -3,6 +3,7 @@ import numpy as np
 import kernels
 import cvxopt
 from cvxopt import matrix
+import time
 
 
 class Classifier :
@@ -10,7 +11,6 @@ class Classifier :
         self.lamb = 1
         self.kernel_name = "linear"
         self.coef = 0
-        self.margin = 0
         self.kernel = self.setKernel(self.kernel_name)
         self.Xtrain = None
         self.predict_func= None
@@ -42,24 +42,29 @@ class Classifier :
         Y = np.array([self.predict_func(X[k]) for k in range(n)])
         return Y
 
-class SVM(Classifier):
-    def __init__(self):
-        super().__init__()
+    def compute_K(self,X):
 
-    def train(self, X, Y):
-        # tcheck if the dimention match
-        shapex = np.shape(X)
-        shapy = np.shape(Y)
-
-        assert shapex[0] == shapy[0]
-
-        n, d = shapex[0], shapex[1]
-        #define the Kernel matrix
+        n = X.shape[0]
         K = np.zeros((n,n))
         for i in range(n):
             for j in range(n):
                 K[i,j] = self.kernel(X[i],X[j])
 
+        return K
+
+
+class SVM(Classifier):
+    def __init__(self):
+        self.margin = 0
+        super().__init__()
+
+    def train(self, X, Y):
+        # tcheck if the dimention match
+
+        K = self.compute_K(X)
+
+        n, d = X.shape
+        assert(len(Y)==n)
         #define the QP
         if self.solver == "quadprog":
 
@@ -92,5 +97,43 @@ class SVM(Classifier):
 
         #solve the QP
 
+        self.Xtrain = X
+        self.predict_func = lambda x : kernels.prediction_function(self.coef, self.Xtrain, self.kernel,x)
+
+class LogisticRegression(Classifier):
+    def __init__(self):
+        super().__init__()
+
+    def logistic(self,x):
+        return(1/(1+np.exp(np.clip(-x,-20,20))))
+
+    def train(self,X,Y):
+        tol = 1e-8
+        K = self.compute_K(X)
+        n ,d = X.shape
+        assert(len(Y)==n)
+
+        self.coef = np.zeros((n,))
+
+        tic = time.time()
+        while(True):
+
+            m = K @ self.coef
+            sqrt_w = np.sqrt(self.logistic(m)*self.logistic(-m))
+            z = m + Y/self.logistic(-Y * m)
+
+            mat_sqrt_W = np.diag(sqrt_w)
+            mat_sqrt_iW = np.diag(1/sqrt_w)
+
+            A = (mat_sqrt_W @ K @ mat_sqrt_W + n * self.lamb * np.eye(n)) @ mat_sqrt_iW
+            b = mat_sqrt_W @ z
+            new_coef = np.linalg.solve(A,b)
+            print(self.logistic(m))
+            if np.all(np.abs(self.logistic(m)-self.logistic(K @ new_coef)) < tol):
+                break
+            else:
+                self.coef = new_coef
+
+        print("Training done in {}s".format(time.time()-tic))
         self.Xtrain = X
         self.predict_func = lambda x : kernels.prediction_function(self.coef, self.Xtrain, self.kernel,x)
